eba98e320b0e65d694efc396f334a9c5368a54b4
Mark Joshua Orcullo <orcullomark856@gmail.com>
2025-03-28 20:11:44 +0800

    fixed following of cursor

--- gun.gd
+++ gun.gd
@@ -5,13 +5,19 @@ var initial_position: float
 func _ready() -> void:
 	initial_position = position.x
 	$AnimatedSprite2D.play("steady")
+	$AnimatedSprite2D.flip_v = false
 
-func _physics_process(delta: float) -> void:
-	var direction := Input.get_axis("left", "right")
+func _process(delta: float) -> void:
+	look_at(get_global_mouse_position())  
+	
+	var direction = get_global_mouse_position().x - global_position.x
+	
 	
 	if direction > 0:
-		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.flip_h = true  
+		$AnimatedSprite2D.flip_v = false
 		$AnimatedSprite2D.position.x = initial_position
 	elif direction < 0:
-		$AnimatedSprite2D.flip_h = false
-		$AnimatedSprite2D.position.x = -90
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.flip_v = true  
+		$AnimatedSprite2D.position.x = -initial_position
--- player.gd
+++ player.gd
@@ -1,23 +1,17 @@
 extends CharacterBody2D
 
-
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
 
-
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
 	var direction := Input.get_axis("left", "right")
-	
+
 	if is_on_floor():
 		if velocity.x == 0:
 			$AnimatedSprite2D.play("idle")
@@ -25,13 +19,15 @@ func _physics_process(delta: float) -> void:
 			$AnimatedSprite2D.play("walk")
 	else:
 		$AnimatedSprite2D.play("jump")
+
+	var mouse_x = get_global_mouse_position().x
+	var player_x = global_position.x
 	
-	
-	
-	if direction > 0:
-		$AnimatedSprite2D.flip_h = true
-	elif direction < 0:
-		$AnimatedSprite2D.flip_h = false
+	if abs(direction) > 0:  
+		$AnimatedSprite2D.flip_h = direction > 0  
+	else:  
+		$AnimatedSprite2D.flip_h = mouse_x > player_x  
+
 	if direction:
 		velocity.x = direction * SPEED
 	else:
