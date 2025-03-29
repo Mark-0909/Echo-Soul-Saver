e6a7d168b43b79d419f1fb5294e390a7e5344b1a
Mark Joshua Orcullo <orcullomark856@gmail.com>
2025-03-29 13:18:32 +0800

    added bullet gone

--- bullet.gd
+++ bullet.gd
@@ -1,6 +1,24 @@
 extends Area2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var timer: Timer = $Timer  # Make sure Timer exists in your scene
+
 const SPEED: int = 300
+var moving: bool = true  # Allow movement initially
 
 func _process(delta: float) -> void:
-	position += transform.x * SPEED * delta
+	if moving:
+		position += transform.x * SPEED * delta  # Bullet moves forward
+
+func _on_body_entered(body: Node2D) -> void:
+	print("Bullet hit:", body.name)  # Debugging output
+
+	# Stop movement and play splash animation
+	moving = false
+	set_physics_process(false)  
+	animated_sprite_2d.play("splash")  
+	
+	await get_tree().create_timer(0.1).timeout
+	
+	queue_free()
+	
