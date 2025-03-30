8c245f8b8f02ce9fad7160230245c56d3562b4a3
Mark Joshua Orcullo <orcullomark856@gmail.com>
2025-03-30 17:26:33 +0800

    final

--- game_manager.gd
+++ game_manager.gd
@@ -5,12 +5,13 @@ var score = 0;
 @onready var player: CharacterBody2D = $"../Player"
 
 @onready var counter: Label = $"../Player/Counter"
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func add_score():
 	score += 1
 	counter.text = "Coins: " + str(score)
 
-
+"stream"
 @onready var timer: Timer = $Timer
 
 func timerstarter() -> void:
--- killzone.gd
+++ killzone.gd
@@ -5,8 +5,9 @@ extends Area2D
 @onready var timer: Timer = $"../gameManager/Timer"
 
 func _on_body_entered(body: Node2D) -> void:
-	print("You died!")
-	timer.start()
-	game_manager.timerstarter()
+	if body.is_in_group("Player"):
+		print("You died!")
+		timer.start()
+		game_manager.timerstarter()
 	
 	
