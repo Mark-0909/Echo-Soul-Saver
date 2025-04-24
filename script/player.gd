0ff93a5e2ed9c67a42c375cef6887a57ca5b39c3
Mark Joshua Orcullo <orcullomark856@gmail.com>
2025-04-24 15:22:44 +0800

    flying effects

--- script/player.gd
+++ script/player.gd
@@ -15,51 +15,54 @@ func _physics_process(delta: float) -> void:
 	if is_transforming:
 		return  # skip movement during transformation
 
-	if not is_on_floor():
+	# Apply gravity only when not ghost
+	if not is_ghost and not is_on_floor():
 		velocity += get_gravity() * delta
+	elif is_ghost:
+		velocity.y = 0  # reset vertical velocity for full control
 
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	# Handle jump (only if not ghost)
+	if Input.is_action_just_pressed("jump") and is_on_floor() and not is_ghost:
 		velocity.y = JUMP_VELOCITY
 
-	var direction := Input.get_axis("left", "right")
+	var direction_x := Input.get_axis("left", "right")
+	var direction_y: float = Input.get_axis("jump", "down") if is_ghost else 0.0
 
-	if is_on_floor():
-		if velocity.x == 0:
-			if is_ghost:
-				$AnimatedSprite2D.play("ghostidle")
-			else:
-				$AnimatedSprite2D.play("idle")
-		elif velocity.x != 0:
-			
-			if is_ghost:
-				$AnimatedSprite2D.play("ghostidle")
-			else:
-				$AnimatedSprite2D.play("walk")
+
+
+	# Animations
+	if direction_x == 0 and direction_y == 0:
+		$AnimatedSprite2D.play("ghostidle" if is_ghost else "idle")
 	else:
 		if is_ghost:
-			$AnimatedSprite2D.play("ghostidle")
+			$AnimatedSprite2D.play("ghostwalk")
+		elif is_on_floor():
+			$AnimatedSprite2D.play("walk")
 		else:
 			$AnimatedSprite2D.play("jump")
-		
 
+	# Flip sprite horizontally
 	var mouse_x = get_global_mouse_position().x
 	var player_x = global_position.x
-
-	if abs(direction) > 0:  
-		$AnimatedSprite2D.flip_h = direction > 0  
-	else:  
-		$AnimatedSprite2D.flip_h = mouse_x > player_x  
-
-	if direction:
-		velocity.x = direction * SPEED
+	if abs(direction_x) > 0:
+		$AnimatedSprite2D.flip_h = direction_x > 0
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$AnimatedSprite2D.flip_h = mouse_x > player_x
+
+	# Apply movement
+	if is_ghost:
+		velocity.x = direction_x * SPEED
+		velocity.y = direction_y * SPEED
+	else:
+		# regular player mode
+		velocity.x = direction_x * SPEED if direction_x else move_toward(velocity.x, 0, SPEED)
 
 	# Handle transform input
 	if Input.is_action_just_pressed("transform") and not is_transforming:
 		await start_transform()
 
 	move_and_slide()
+
 
 # -- TRANSFORMATION LOGIC --
 func start_transform() -> void:
