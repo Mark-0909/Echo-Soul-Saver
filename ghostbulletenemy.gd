8beb0a2ac291b63d417389e92851c04a2a1fdf88
Mark Joshua Orcullo <orcullomark856@gmail.com>
2025-04-27 19:06:59 +0800

    added ghost

---
+++ ghostbulletenemy.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+const SPEED: int = 300
+var moving: bool = true  
+
+@export var is_player_bullet: bool = true
+
+@export var is_ghost_bullet: bool = true
+
+func _process(delta: float) -> void:
+	if moving:
+		position += transform.x * SPEED * delta  
+
+func _on_body_entered(body: Node2D) -> void:
+	
+	if body.is_in_group("Physical") and body.is_in_group("Enemy") and is_ghost_bullet and is_player_bullet:
+		return
+	if body.is_in_group("Player") and is_player_bullet and is_ghost_bullet:
+		return
+		
+	if body.is_in_group("Ghost") and body.is_in_group("Enemy") and is_ghost_bullet and is_player_bullet:
+		if body.is_in_group("Enemy"):
+			if body.is_in_group("Ghost"):
+				print("Enemy is a ghost — bullet passed through")
+				return  # Ignore hitting ghost enemies
+			
+			if body.has_method("LoseLife"):
+				body.LoseLife()
+	# Handle bullet impact visuals
+	moving = false 
+	set_process(false)  
+	animated_sprite_2d.play("splash")  
+	await get_tree().create_timer(0.1).timeout  
+	queue_free()
