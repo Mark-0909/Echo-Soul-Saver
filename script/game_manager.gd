ee04813ffbb616ce1c1deae83caf18856d67582c
Mark Joshua Orcullo <orcullomark856@gmail.com>
2025-04-29 23:29:46 +0800

    added maimenu

--- script/game_manager.gd
+++ script/game_manager.gd
@@ -4,12 +4,11 @@ var score = 0;
 
 @onready var player: CharacterBody2D = $"../Player"
 
-@onready var counter: Label = $"../Player/Counter"
 @onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
 
 func add_score():
 	score += 1
-	counter.text = "Coins: " + str(score)
+
 
 "stream"
 @onready var timer: Timer = $Timer
